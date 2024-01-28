330def96ceb3a0fd2033946aba225ba76778e293
joesenny <est2020.joe@gmail.com>
2024-01-28 00:42:22 -0600

    On branch main  Initial commit  Changes to be committed: 	new file:   csharp/lexer/.gitignore 	new file:   csharp/lexer/Program.cs 	new file:   csharp/lexer/lexer.csproj  Untracked files:

---
+++ csharp/lexer/Program.cs
@@ -0,0 +1,28 @@
+﻿// lexer in c# for to parse HTML
+
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace lexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+          {
+            string filename = args[0];
+            string text = File.ReadAllText(filename);
+            Lexer lexer = new Lexer(text);
+            Token token = lexer.nextToken();
+            while (token.type != TokenType.EOF)
+            {
+                Console.WriteLine(token);
+                token = lexer.nextToken();
+            }
+        }   
+    }
+
+    enum TokenType
+    
+
